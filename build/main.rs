144430cbb3b71a57c63a0c2dd294534b38252f09
Jordan Paige Hendricks <jordan@oxide.computer>
2026-03-20 22:21:35 +0000

    remove dependency on ipcc

--- build/main.rs
+++ build/main.rs
@@ -9,19 +9,7 @@ use anyhow::Result;
 #[cfg(feature = "test-data")]
 mod test_data;
 
-/// This path is where Oxide specific libraries live on helios systems.
-/// The linker needs this path to find libipcc:
-/// https://github.com/oxidecomputer/ipcc-rs/
-#[cfg(target_os = "illumos")]
-static OXIDE_PLATFORM: &str = "/usr/platform/oxide/lib/amd64/";
-
 fn main() -> Result<()> {
-    #[cfg(target_os = "illumos")]
-    {
-        println!("cargo:rustc-link-arg=-Wl,-R{}", OXIDE_PLATFORM);
-        println!("cargo:rustc-link-search={}", OXIDE_PLATFORM);
-    }
-
     #[cfg(feature = "test-data")]
     test_data::generate().context("generate mock data for testing")?;
 
